fb200dfad57e602c0f3704b800a94e1512ab733c
Teemu <teemu.harjula@student.samk.fi>
2025-01-31 11:08:13 +0200

    Add simple_sub.py

---
+++ src/simple_pub_sub/src/simple_sub.py
@@ -0,0 +1,20 @@
+#! /usr/bin/python3
+
+import rospy
+from std_msgs.msg import String
+
+topic_to_listen = 'simple_pub'
+
+def cb(msg):
+    print(f'I heard | {msg} | from {topic_to_listen}')
+    print('-----')
+
+rospy.init_node('listener')
+sub = rospy.Subscriber(topic_to_listen, String, cb)
+rate = rospy.Rate(1)
+
+rospy.spin()
+
+# while not rospy.is_shutdown():
+#     print(sub.callback)
+#     rate.sleep()
