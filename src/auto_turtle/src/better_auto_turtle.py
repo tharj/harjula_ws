bee6bab6f972f252289100d14b42eb9d10157791
Teemu <teemu.harjula@live.com>
2025-03-09 17:15:30 +0200

    autoturtle rework

--- src/auto_turtle/src/better_auto_turtle.py
+++ src/auto_turtle/src/better_auto_turtle.py
@@ -1,67 +1,55 @@
 #! /usr/bin/python3
 
-'''
-Theta is the angle of the turtle in radians, 
-    theta = 0, looking right
-        from 0 to pi, looking up, pi/2 being straight up
-    theta = pi or negative pi, looking left
-        from 0 to negative pi, looking down, -pi/2 being straight down
-'''
-
 import rospy
-from turtlesim.msg import Pose 
-from geometry_msgs.msg import Twist 
+from turtlesim.msg import Pose
+from geometry_msgs.msg import Twist
+from math import pi
 import random
 
-pi = 3.141592653589793
+turning = 0
+start_time = None
+
 
 def cb(msg):
+    global start_time, turning
+
     vel_msg = Twist()
 
-    vel_msg.linear.x = 3
-    vel_msg.angular.z = random.uniform(-1,1)
-    turn_speed = 0
-    random_angle = random.uniform(-0.5,0.5)
+    near_wall = False
+    random_speed = 0
 
-    # calculate turn speeds for different orientations
-    if msg.x > 10: # RIGHT
-        if msg.theta < 0: # downward
-            turn_speed = -(pi-msg.theta) + random_angle
-        else: # upward
-            turn_speed = pi-msg.theta + random_angle
-    elif msg.y > 10: # BOTTOM
-        turn_speed = -msg.theta + random_angle
-    elif msg.x < 1: # LEFT
-        if msg.theta < 0: # downward
-            turn_speed = -(pi-msg.theta) + random_angle
-        else: # upward
-            turn_speed = pi-msg.theta + random_angle
-    elif msg.y < 1: # TOP
-        turn_speed = -msg.theta + random_angle
-    else:
-        turn_speed = 0
+    if msg.x > 10 or msg.y > 10 or msg.x < 1 or msg.y < 1: # check wall vicinity from coordinates
+        near_wall = True # set flag
 
-    if turn_speed != 0: # if a wall detection has assigned a turn speed
-            turning = True
-            while turning:    
-                vel_msg.linear.x = 0 # stop linear movement
-                vel_msg.angular.z = turn_speed # set angular speed from the previous pose theta
-                velocity_publisher.publish(vel_msg) # publish speeds
-                rospy.loginfo(f'msg.x: {msg.x}')
-                # rospy.sleep(1) # sleep 1 sec to turn
-                if abs(msg.theta - turn_speed) < 0.2:
-                    turn_speed = 0
-                    turning = False
-                    vel_msg.angular.z = 0
-                    vel_msg.linear.x = 3 
-                    velocity_publisher.publish(vel_msg)
-            # rospy.sleep(0.1) # allow time to move away from the wall, to not trigger another turn_speed assignment immediately
+    if turning == 0 and near_wall: # if not yet turning, but close to wall
+        turning = 1 # set turn flag
+        start_time = rospy.get_time() # start time for comparison later 
+        rospy.loginfo('Start turning') # debug
+        random_speed = random.uniform(-0.7, 0.7) 
+
+    if turning == 1: 
+        vel_msg.linear.x = 0.0  # Stop moving forward
+        vel_msg.angular.z = pi + random_speed # set angular velocity to turn
+
+        elapsed_time = rospy.get_time() - start_time
+        rospy.loginfo(f'Elapsed time: {elapsed_time}')
+
+        if elapsed_time > 1.0: # rospy.sleep doesnt work
+            turning = 0 # flag down
+            vel_msg.linear.x = 2.0 # linear move back up
+            vel_msg.angular.z = random.uniform(-1, 1)  # Random angular speed
+            rospy.loginfo('Turning completed')
+
+    else: # default linear moving state
+        vel_msg.linear.x = 2.0 
+        vel_msg.angular.z = -4 if (random.uniform(-1, 1) < 0) else 4 
 
     velocity_publisher.publish(vel_msg)
 
+
+# Initialize node, publisher, and subscriber
 rospy.init_node('auto_turtle')
-velocity_publisher = rospy.Publisher('/turtle1/cmd_vel', Twist, queue_size=1) 
+velocity_publisher = rospy.Publisher('/turtle1/cmd_vel', Twist, queue_size=1)
 rospy.Subscriber('/turtle1/pose', Pose, cb)
 
-while not rospy.is_shutdown():
-    rospy.spin()
+rospy.spin()
---
+++ src/auto_turtle/src/better_auto_turtle2.py
@@ -0,0 +1,67 @@
+#! /usr/bin/python3
+
+'''
+Theta is the angle of the turtle in radians, 
+    theta = 0, looking right
+        from 0 to pi, looking up, pi/2 being straight up
+    theta = pi or negative pi, looking left
+        from 0 to negative pi, looking down, -pi/2 being straight down
+'''
+
+import rospy
+from turtlesim.msg import Pose 
+from geometry_msgs.msg import Twist 
+import random
+
+pi = 3.141592653589793
+
+def cb(msg):
+    vel_msg = Twist()
+
+    vel_msg.linear.x = 3
+    vel_msg.angular.z = random.uniform(-1,1)
+    turn_speed = 0
+    random_angle = random.uniform(-0.5,0.5)
+
+    # calculate turn speeds for different orientations
+    if msg.x > 10: # RIGHT
+        if msg.theta < 0: # downward
+            turn_speed = -(pi-msg.theta) + random_angle
+        else: # upward
+            turn_speed = pi-msg.theta + random_angle
+    elif msg.y > 10: # BOTTOM
+        turn_speed = -msg.theta + random_angle
+    elif msg.x < 1: # LEFT
+        if msg.theta < 0: # downward
+            turn_speed = -(pi-msg.theta) + random_angle
+        else: # upward
+            turn_speed = pi-msg.theta + random_angle
+    elif msg.y < 1: # TOP
+        turn_speed = -msg.theta + random_angle
+    else:
+        turn_speed = 0
+
+    if turn_speed != 0: # if a wall detection has assigned a turn speed
+            turning = True
+            while turning:    
+                vel_msg.linear.x = 0 # stop linear movement
+                vel_msg.angular.z = turn_speed # set angular speed from the previous pose theta
+                velocity_publisher.publish(vel_msg) # publish speeds
+                rospy.loginfo(f'msg.x: {msg.x}')
+                # rospy.sleep(1) # sleep 1 sec to turn
+                if abs(msg.theta - turn_speed) < 0.2:
+                    turn_speed = 0
+                    turning = False
+                    vel_msg.angular.z = 0
+                    vel_msg.linear.x = 3 
+                    velocity_publisher.publish(vel_msg)
+            # rospy.sleep(0.1) # allow time to move away from the wall, to not trigger another turn_speed assignment immediately
+
+    velocity_publisher.publish(vel_msg)
+
+rospy.init_node('auto_turtle')
+velocity_publisher = rospy.Publisher('/turtle1/cmd_vel', Twist, queue_size=1) 
+rospy.Subscriber('/turtle1/pose', Pose, cb)
+
+while not rospy.is_shutdown():
+    rospy.spin()
